588956136a1176ed683e7efd74992fcf4131d9ea
Marco Ieni <11428655+MarcoIeni@users.noreply.github.com>
2024-04-23 16:27:20 +0200

    fix

--- worker_a/src/worker.ts
+++ worker_a/src/worker.ts
@@ -2,7 +2,7 @@ import { WorkerEntrypoint } from "cloudflare:workers";
 
 export class MyClass extends WorkerEntrypoint {
   async fetch() {
-    return new Response("Hello from Worker B");
+    return new Response("Hello from Worker A");
   }
 
   add(a: number, b: number) {
